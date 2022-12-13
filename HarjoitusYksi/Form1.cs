566dc55b515f3aab34cef5b8aef393087450da06
Niklas Vuorio <niklas.vuorio@protonmail.com>
2022-12-13 10:15:13 +0200

    Vaihdoin Form1.cs tiedostosta OtsikkoLB.Text muttujaa

--- HarjoitusYksi/Form1.cs
+++ HarjoitusYksi/Form1.cs
@@ -9,7 +9,7 @@ namespace HarjoitusYksi
 
         private void VaihdaBT_Click(object sender, EventArgs e)
         {
-            OtsikkoLB.Text = "Heippa Niklas";
+            OtsikkoLB.Text = "Heippa Maailma";
         }
     }
 }
